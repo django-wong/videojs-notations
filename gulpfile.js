19ca84d9eccadda8981a58d6af10908bc463024b
Django Wong <383126908@qq.com>
2017-05-17 03:33:21 +0800

    remove useless dist path from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 * @Author: Django Wong
 * @Date:   2017-04-27 17:49:31
 * @Last Modified by:   Django Wong
-* @Last Modified time: 2017-04-28 00:21:31
+* @Last Modified time: 2017-05-17 03:32:05
 * @File Name: gulpfile.js
 */
 
@@ -16,15 +16,13 @@ const $ = gulpLoadPlugins();
 gulp.task('build', function(){
 	return gulp.src('src/*.js').
 		pipe($.babel()).
-		pipe(gulp.dest('dist/')).
-		pipe(gulp.dest('/Users/DjangoWong/Workspace/VideoPlayer/'));
+		pipe(gulp.dest('dist/'));
 });
 
 gulp.task('less', function(){
 	return gulp.src('src/*.less').
 		pipe($.less()).
-		pipe(gulp.dest('dist/')).
-		pipe(gulp.dest('/Users/DjangoWong/Workspace/VideoPlayer/'));
+		pipe(gulp.dest('dist/'));
 });
 
 gulp.task('watch', function(){
